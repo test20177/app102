e1568783d2f06b8ec1ab8fe29f53e7c3872525ba
kit <kit@kit.com>
2020-03-31 10:46:22 +0100

    Updates

---
+++ precache-manifest.b40d454e6d211a1ce86551693fdd9e93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3008e956872fe86d551978d8cc8a31b",
+    "url": "/app102/index.html"
+  },
+  {
+    "revision": "7914397326e76e472ff0",
+    "url": "/app102/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f12a85d9645adb255e9",
+    "url": "/app102/static/js/2.30cf3b20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app102/static/js/2.30cf3b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7914397326e76e472ff0",
+    "url": "/app102/static/js/main.14474935.chunk.js"
+  },
+  {
+    "revision": "03b42d19a622bb7fd4c0",
+    "url": "/app102/static/js/runtime-main.1bd5a734.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app102/static/media/logo.5d5d9eef.svg"
+  }
+]);
